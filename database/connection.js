d686855cf45232ff014da27e539e002ae585680a
Claudena Daniel <cjdaniel0530@gmail.com>
2026-07-13 19:51:03 -0400

    added error message to identify actual error

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (err) {
-        console.error("MongoDB connection error");
+        console.error("MongoDB connection error:", err.message); //adding so i can see what the actual error is
     }
 };
 
