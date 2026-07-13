2cc8a6a9d7e77f4291dea1cc3f2debef06518ab8
Claudena Daniel <cjdaniel0530@gmail.com>
2026-07-12 20:05:53 -0400

    moved connectDB outside of appListen

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
-import connectDB from "./database/connection";
+import connectDB from "./database/connection.js";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 
+dotenv.config(); //put before connectDB
+connectDB();
+
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    connectDB();
-})
+    
+});
