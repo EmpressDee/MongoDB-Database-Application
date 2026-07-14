05db600fac3f2da460a71a4c9ab5006faf468340
Claudena Daniel <cjdaniel0530@gmail.com>
2026-07-13 23:00:17 -0400

    added delete client route

--- controllers/clientcontroller.js
+++ controllers/clientcontroller.js
@@ -43,4 +43,19 @@ export const createClient = async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
     }
- }
+ };
+
+ //delete
+
+ export const deleteClient = async (req,res) =>{
+    try {
+        const deleted = await Client.findByIdAndDelete(req.params.id);
+        if (!deleted) return res.status(404).json({ error: "Client not found" });
+    res.status(200).json({ message: "Client deleted", deleted });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
+
+    };
+ 
